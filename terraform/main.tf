9c4729499a28b2d47ebe3c6606a8985e6c2facde
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2025-10-25 21:52:16 -0400

    lower geowidth (#173)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.10.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
@@ -12,15 +12,16 @@ terraform {
     }
     archive = {
       source  = "hashicorp/archive"
-      version = "~> 2.4"
+      version = "~> 2.7"
     }
   }
 
   backend "s3" {
-    bucket  = "mvana-account-terraform"
-    key     = "aggregator/state.json"
-    region  = "us-east-1"
-    profile = "mvana"
+    bucket       = "mvana-account-terraform"
+    key          = "aggregator/state.json"
+    use_lockfile = true
+    region       = "us-east-1"
+    profile      = "mvana"
   }
 }
 
